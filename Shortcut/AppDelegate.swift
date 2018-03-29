9d73a654f55f52e236b8d108a7dc5d750ecaecbb
Specia1ist17 <arocho.fernando.13@gmail.com>
2018-03-29 12:13:13 -0400

    Connected 3D touch action to specific view controller

--- Shortcut/AppDelegate.swift
+++ Shortcut/AppDelegate.swift
@@ -12,11 +12,47 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var vcsArray = [UIViewController]()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let mountainsVC = storyboard.instantiateViewController(withIdentifier: "mountainsVC") as! MountainsVC
+        let spaceVC = storyboard.instantiateViewController(withIdentifier: "spaceVC") as! SpaceVC
+        let oceanVC = storyboard.instantiateViewController(withIdentifier: "oceanVC") as! OceanVC
+        
+        vcsArray = [mountainsVC, spaceVC, oceanVC]
         return true
+    }
+    
+    enum ShortcutType: String {
+        case mountains = "mountains"
+        case space = "space"
+        case ocean = "ocean"
+    }
+    
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        if let type = shortcutItem.type.components(separatedBy: ".").last {
+            
+            let navVC = window?.rootViewController as! UINavigationController
+            navVC.setViewControllers(vcsArray, animated: false)
+            
+            switch type {
+            case ShortcutType.space.rawValue:
+                navVC.popToViewController(vcsArray[1], animated: true)
+                completionHandler(true)
+            case ShortcutType.ocean.rawValue:
+                navVC.popToViewController(vcsArray[2], animated: true)
+                completionHandler(true)
+            default:
+                navVC.popToRootViewController(animated: true)
+                completionHandler(true)
+                print("hola")
+            }
+        }
+        
+        completionHandler(false)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
